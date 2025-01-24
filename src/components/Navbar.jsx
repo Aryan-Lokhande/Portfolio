f5d7c22b50cf71d91cee55893e19a9db5f197c5a
Aryan Lokhande <aryanlok619@gmail.com>
2025-01-24 20:47:00 +0530

    Update Navbar.jsx
    home active has been remove

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ function Navbar() {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <a className="nav-link active" href="#home">Home</a>
+              <a className="nav-link" href="#home">Home</a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#about">About Me</a>
